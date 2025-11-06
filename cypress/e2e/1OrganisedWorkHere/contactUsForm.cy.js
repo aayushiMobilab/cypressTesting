68e3668eb1ede875fff3ddf4958bf134390ccda8
Aayushi Dhariwal <124355566+aayushiMobilab@users.noreply.github.com>
2025-11-06 10:21:16 +0100

    Enhance contact form test with file upload and comments
    Updated the contact form test to use a file upload and added TODO comments for improvements.

--- cypress/e2e/1OrganisedWorkHere/contactUsForm.cy.js
+++ cypress/e2e/1OrganisedWorkHere/contactUsForm.cy.js
@@ -9,11 +9,21 @@ describe("User should be able to write a ContactUsForm and can subscribe to News
     cy.get('[data-qa="message"]').type(
       "Hello, I want to talk to you about my recent Job Interview! Hope to hear from you again. Greetings Jonathan"
     );
+    //TODO: Use relative path meaning .selectFile("cypress/fixtures/TesterTextToUpload.txt")
     cy.get("input[type=file]").selectFile(
       "/Users/alexandergschwendtner/Documents/cypress-learning/cypress/fixtures/TesterTextToUpload.txt"
     );
     cy.get('[data-qa="submit-button"]').click();
     // Verifies that the Message got sent
+    /**
+     * TODO: This is duplicate. You can do something like
+     * cy.get(".status")
+        .should("be.visible")
+        .and(
+          "contain.text",
+          "Success! Your details have been submitted successfully.",
+        )
+     */
     cy.contains(
       "Success! Your details have been submitted successfully."
     ).should(
